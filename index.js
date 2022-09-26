bcf5250e478421bf6bc6bb0ad23021beb41299ba
CAS-CAS-CAS <247Adventurer@gmail.com>
2022-09-26 12:20:22 -0700

    welcome back

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ var Sentencer = require('sentencer');
 
 Sentencer.configure({
     nounList:['artificer', 'barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger','rogue', 'sorcerer', 'warlock', 'wizard'],
-    adjectiveList: ['strong', 'powerful', 'nimble', 'wise','charming', 'clever'],
+    adjectiveList: ['strong', 'powerful', 'nimble', 'wise','charming', 'clever', 'speedy', 'witty', 'burly','elated', 'elegant', 'oblivious', 'awesome'],
     actions:{}    
 });
-presentVerbs = ['enters', 'journeys' , 'ventures', 'travels', 'parades']
+presentVerbs = ['enters', 'journeys' , 'ventures', 'travels', 'parades', 'struts', 'traverses', 'descends']
 
-console.log(Sentencer.make(`{{adjective}} {{ noun }} ${presentVerbs[Math.floor(Math.random()* presentVerbs.length)]}`))
-console.log(Sentencer.make("{{ noun }}"))
+// check sentence for noun afterwards
+//split the function up to only call  ase 1 a or case 2 an (expected behavoir)
+// solution 3?? then
+for(let i = 0; i < 10; i++){
+    console.log(Sentencer.make(`{{an_adjective}} {{ noun }} ${presentVerbs[Math.floor(Math.random()* presentVerbs.length)]}`))
+}
 
 
 
